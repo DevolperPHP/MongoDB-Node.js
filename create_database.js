5b7f420b1c81044634361e76b78aba1ab042578d
DevolperPHP <devolperphp@gmail.com>
2020-05-14 11:57:52 +0300

    Update create_database.js

--- create_database.js
+++ create_database.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var data = "mongodb://localhost:27017/database";
 
-MongoClient.connect(data, function(err, db) {
+MongoClient.connect(data, { useUnifiedTopology: true }, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   db.close();
